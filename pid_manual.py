6b74449a6f255b531829a10ddf14f8b4f9759b50
Ben Li <benjamin.li@duke.edu>
2022-11-15 08:56:51 -0500

    Added pygame stuff

--- pid_manual.py
+++ pid_manual.py
@@ -1,31 +1,44 @@
 import math
 import random
 import pygame
+from pygame.locals import *
 
-P = 0.5
-I = 0
+#set up pygame and constants
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((500, 600))
+c = 2 # scale factor
+
+# pid tuning parameters
+P = 0.75
+I = 0.0140
 D = 0
 
+# map model parameters
 b0 = 0.18661
 bm = 0.07464
 a1 = 0.74082
 m = 3
 d = 3
 
+# stores 10 most recent infusion rates
 infusion_log = []
 for i in range(0, 10):
     infusion_log.append(0)
     
+# define variables for blood pressure
 bp_log = []
-map = 115
-target_map = 65
+map = 100
+target_map = 140
 previous_map_change = 0
 map_change = 0
 
-for i in range(0, 10):
+# store 100 most recent map values
+for i in range(0, 100):
     bp_log.append(map)
 
-def plant(infusion):
+# patient response to infusion rate
+def response(infusion):
     global map
     global infusion_log
     global map_change
@@ -34,22 +47,40 @@ def plant(infusion):
     print("new infusion: ", infusion)
     infusion_log.append(infusion)
     infusion_log.pop(0)
-    map_change = b0 * infusion_log[9 - d]+ bm * infusion_log[9 - d - m] + a1 * previous_map_change
+    map_change = b0 * infusion_log[9 - d]+ bm * infusion_log[9 - d - m] + a1 * previous_map_change + random.randrange(-5, 5)
     map = map - map_change
     bp_log.append(map)
     bp_log.pop(0)
     
-def calc_error(map):
-    return target_map - map
-
 integral = 0
 derivative = 0
-for t in range(0, 100):
+timestep = 2
+t = 0
+done = False
+while not done:
+    clock.tick(timestep)
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                done = True
     # t is in seconds
     error = map - target_map
-    integral += error
-    derivative = bp_log[9] - bp_log[8]
+    integral += error * timestep
+    derivative = (bp_log[len(bp_log) - 1] - bp_log[len(bp_log) - 2]) / timestep
     control = P * error + I * integral + D * derivative
-    plant(control)
+    response(control)
     print("time: ", t)
     print("map: ", map)
+    print("most recent bp: ", bp_log[len(bp_log) - 1])
+    t += 1
+    
+    #lower boundary
+    pygame.draw.line(screen, (255, 0, 0), (0,(500-c*60)), ((500, (500-c*60))))
+    #upper boundary
+    pygame.draw.line(screen, (255, 0, 0), (0,(500-c*80)), ((500, (500-c*80))))
+    
+    #iterates and draws line between bp points
+    for i in range(len(bp_log) - 1):
+        pygame.draw.line(screen, (0, 128, 255), (int(i * 2 * c),(500-c * bp_log[i])), (int((i + 1) * 2 * c), (500-c * bp_log[i+1])))
+        
+    pygame.display.flip()
